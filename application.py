b7db6c399c1860d91c63ef2663f43bcdd10d95d6
ericxpy <pxu9@jhu.edu>
2019-03-02 17:51:11 -0500

    first commit

---
+++ application.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask import render_template
+from flask import request   
+import traceback  
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('hello.html')
+  
+
+if __name__ == '__main__':
+    app.run(debug=True)
